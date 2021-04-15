669c3f0750f0e530b44e0d7bb848ffc9f4710692
Sean Templeton <sean@templeton.io>
2021-04-14 20:39:34 -0500

    Moved resources

--- Package.swift
+++ Package.swift
@@ -5,11 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "DateRangePicker",
+    defaultLocalization: "en",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DateRangePicker",
-            targets: ["DateRangePicker"]
+            targets: ["DateRangePicker"],
+            resources: [
+                .process("Resources")
+            ]
         ),
     ],
     dependencies: [
